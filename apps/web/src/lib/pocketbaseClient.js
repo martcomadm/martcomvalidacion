bf0984279f57104f6f0eb2063d37698897169a1c
martcomadm <martcomadm@gmail.com>
2026-04-21 09:46:48 -0600

    Update pocketbaseClient.js

--- apps/web/src/lib/pocketbaseClient.js
+++ apps/web/src/lib/pocketbaseClient.js
@@ -1,6 +1,6 @@
 import Pocketbase from 'pocketbase';
 
-const POCKETBASE_API_URL = "/hcgi/platform";
+const POCKETBASE_API_URL = "https://martcom-validaciones-api.mchc0z.easypanel.host";
 
 const pocketbaseClient = new Pocketbase(POCKETBASE_API_URL);
 
